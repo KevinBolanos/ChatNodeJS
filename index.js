2a66ddea369a8f4ba787eda1e7fb027c2820bf14
Kevin Alarcon <kevin.alarcon@alumnos.udg.mx>
2020-03-11 15:41:58 -0600

    Base guide & Heroku deploy

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var socket = require('socket.io');
+var port = process.env.PORT || 4000
+
+var app = express();
+var server = app.listen(port, function(){
+    console.log('escuchando las peticiones');
+});
+
+app.use(express.static('public'));
+
+var io = socket(server);
+
+io.on('connection', function(socket){
+    console.log('conexión socket', socket.id);
+
+    socket.on('chat', function(data){
+        io.sockets.emit('chat', data);
+    });
+
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
+});
---
+++ public/chat.js
@@ -0,0 +1,32 @@
+﻿var socket = io();
+
+var message = document.getElementById('message');
+    handle = document.getElementById('handle'),
+    btn = document.getElementById('send'),
+    output = document.getElementById('output'),
+    feedback = document.getElementById('feedback');
+
+
+btn.addEventListener('click', function(){
+    socket.emit('chat', {
+        message: message.value,
+        handle: (handle.value === '')?'Anónimo':handle.value
+    });
+    message.value = '';
+});
+
+message.addEventListener('keypress', function(){
+    socket.emit('typing', handle.value);
+});
+
+socket.on('chat', function(data){
+    feedback.innerHTML = "";
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+    output.scrollIntoView(false); 
+});
+
+socket.on('typing', function(data){
+    if(data === "") data = 'Anónimo';
+    feedback.innerHTML = '<p><em>' + data + ' está escribiendo...</em></p>';
+    feedback.scrollIntoView(false);
+});
